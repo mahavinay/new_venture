14e14eb992dfc63005ea59f2499a39644562fac7
mahavinay <mahavinay28@gmail.com>
2018-09-27 14:03:55 +0200

    updated with function to display cards

--- cardGame.js
+++ cardGame.js
@@ -1,5 +1,5 @@
 // Display cards in the Card Game
-//Dated on 25.09.18
+//Updated on 25.09.18
 
 console.log
 ("Welcome to Cards Game, let us the see the cards available in the Card Game");
@@ -8,17 +8,37 @@ let cardShapes = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let otherCards = ['King', 'Queen', 'Jack', 'Ace','Ten'
                   ,'Nine', 'Eight', 'Seven', 'Six', 'Five'
                   ,'Four', 'Three', 'Two'];
-let deckOfCards = [];
+//let deckOfCards = [];
 
-for (let cardIdx = 0; cardIdx < cardShapes.length; cardIdx++){
-  for (otherIdx = 0; otherIdx < otherCards.length; otherIdx++){
-    deckOfCards.push(otherCards[otherIdx] + ' of ' + cardShapes[cardIdx]);
-  }
+function createDeck(){
+    let deckOfCards = [];
+    for (let cardIdx = 0; cardIdx < cardShapes.length; cardIdx++){
+        for (otherIdx = 0; otherIdx < otherCards.length; otherIdx++){
+            deckOfCards.push(otherCards[otherIdx] + ' of ' + cardShapes[cardIdx]);
+            }
+    }
+    return deckOfCards;
+
 }
 
-for ( let deckIdx = 0; deckIdx < deckOfCards.length; deckIdx++ ){
+
+  //console.log("Function pick");
+  let deckOfCards = createDeck();
+
+  for ( let deckIdx = 0; deckIdx < deckOfCards.length; deckIdx++ ){
   //console.log(deckOfCards);
   console.log (deckOfCards[deckIdx]);
+  }
+
+function pickNextCard()
+{
+  return deckOfCards.shift();
 }
 
-console.log(deckOfCards);
+//pickNextCard();
+
+
+let PlayerCards= [pickNextCard(),pickNextCard()];
+console.log ("You Picked"+" " +PlayerCards[0] );
+console.log ("You Picked"+" " +PlayerCards[1] );
+//console.log(deckOfCards);
