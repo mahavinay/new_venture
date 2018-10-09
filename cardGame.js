034843cd7a371cc08a434a96e2d37e0036c5252a
mahavinay <mahavinay28@gmail.com>
2018-10-09 08:40:20 +0200

    setting up the game

--- cardGame.js
+++ cardGame.js
@@ -10,19 +10,43 @@ let otherCards = ['King', 'Queen', 'Jack', 'Ace','Ten'
                   ,'Four', 'Three', 'Two'];
 //let deckOfCards = [];
 
+
+//DOM Variables
 let textArea = document.getElementById('text-area');
 let newGameButton = document.getElementById('new-game');
 let hitButton = document.getElementById('hit-button');
 let stayButton = document.getElementById('stay-button');
 
+// Game Variables
+let gameStarted = false,
+    gameOver = false,
+    PlayerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    deck = [],
+    dealerScore = 0,
+    playerScore = 0;
+
+
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
+showStatus();
 
 newGameButton.addEventListener('click', function(){
-  textArea.innerText = 'Game Started';
+  gameStarted = true;
+  gameOver = false;
+  PlayerWon = false;
+
+  deckOfCards = createDeck();
+  shuffleDeck(deckOfCards);
+  dealerCards = [pickNextCard(),pickNextCard()];
+  playerCards = [pickNextCard(),pickNextCard()];
+
   newGameButton.style.display='none';
+  showStatus();
   hitButton.style.display='inline';
   stayButton.style.display='inline';
+
 });
 
 function createDeck(){
@@ -43,7 +67,7 @@ function createDeck(){
 
 
   //console.log("Function pick");
-  let deckOfCards = createDeck();
+ //let deckOfCards = createDeck();
 
 function getCardString(stackCard)
 {
@@ -60,10 +84,39 @@ function pickNextCard()
   return deckOfCards.shift();
 }
 
+function showStatus()
+{
+  if(!gameStarted){
+    textArea.innerText = 'Welcome to Card Game';
+    return;
+  }
+
+  for ( let i=0; i<deckOfCards.length;i++)
+  {
+    textArea.innerText += '\n'+getCardString(deckOfCards[i]) ;
+  }
+}
+
+function shuffleDeck(deckOfCards)
+{
+  for (let i = 0; i<deckOfCards.length;i++)
+  {
+
+    let swapIdx = Math.trunc(Math.random()*deckOfCards.length);
+    let tmp =  deckOfCards[swapIdx];
+    deckOfCards[swapIdx] = deckOfCards[i];
+    deckOfCards[i] = tmp;
+    console.log(tmp)
+  }
+
+  //return;
+}
+
+
 //pickNextCard();
 
 
-let PlayerCards= [pickNextCard(),pickNextCard()];
-console.log ("You Picked"+" " +getCardString(PlayerCards[0]) );
-console.log ("You Picked"+" " +getCardString(PlayerCards[1]) );
+//let PlayerCards= [pickNextCard(),pickNextCard()];
+//console.log ("You Picked"+" " +getCardString(PlayerCards[0]) );
+//console.log ("You Picked"+" " +getCardString(PlayerCards[1]) );
 //console.log(deckOfCards);
